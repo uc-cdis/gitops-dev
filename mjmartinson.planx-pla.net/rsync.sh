ed9e05220b85f1a36572a3d9332c1a223337fd30
Andrew Prokhorenkov <aprokh@uchicago.edu>
2022-07-13 14:35:37 -0500

    feat: remove old environments

--- mjmartinson.planx-pla.net/rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-folder="$(dirname "$(realpath "$0")")"
-command=(rsync -avz --exclude '*/data-volume/*' "${folder}/" mjmartinson@cdistest.csoc:cdis-manifest/mjmartinson.planx-pla.net/)
-echo "${command[@]}"
-${command[@]}
--- reuben.planx-pla.net/rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-folder="$(dirname "$(realpath "$0")")"
-command=(rsync -avz --exclude '*/data-volume/*' "${folder}/" reuben@cdistest.csoc:cdis-manifest/reuben.planx-pla.net/)
-echo "${command[@]}"
-${command[@]}
